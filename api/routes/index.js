193f8a03b33034fe27d293a51780d2998828a147
EnzoCozza <cozzaenzo@gmail.com>
2020-10-05 18:05:12 -0300

    Added route for proposals

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,14 +1,11 @@
-const test = require('./test');
-const election = require('./election');
-const option = require('./option');
+const test = require("./test");
+const election = require("./election");
+const option = require("./option");
+const proposal = require("./proposal");
 
 module.exports = {
-    name: 'routes',
-    register: server => {
-      return server.route([
-        ...test,
-        ...election,
-        ...option,
-      ]);
-    },
-  };
+  name: "routes",
+  register: (server) => {
+    return server.route([...test, ...election, ...option, ...proposal]);
+  },
+};
