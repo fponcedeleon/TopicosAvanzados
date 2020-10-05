9464540493d3499e8a1fec5f528b24061c514a46
EnzoCozza <cozzaenzo@gmail.com>
2020-10-05 15:30:48 -0300

    Added methods to fetch api

---
+++ web/scripts/services/election.js
@@ -0,0 +1,52 @@
+import { post, get, put, deleteApi } from "../utils/api.js";
+import { removePostId } from "../utils/session.js";
+
+// const baseUrl =
+//   window.location.hostname === '169.254.175.166'
+//     ? 'http://localhost:3000'
+//     : 'https://limitless-everglades-76624.herokuapp.com';
+const baseUrl =
+  window.location.hostname === "localhost"
+    ? "http://localhost:8000"
+    : "http://localhost:8000"; //ACA VA HEROKU CUANDO ESTE ANDANDO
+
+export const createNewElection = async (
+  createdBy,
+  startDate,
+  endDate,
+  isActive,
+  minAge,
+  maxAge,
+  city,
+  country
+) => {
+  const { data: election, error } = await post(`${baseUrl}/elections`, {
+    createdBy,
+    startDate,
+    endDate,
+    isActive,
+    minAge,
+    maxAge,
+    city,
+    country,
+  });
+  if (error) {
+    if (error.status === 409) {
+      throw new Error("Election already exists.");
+    }
+    throw error;
+    //throw new Error('Oops! Something went wrong...');
+  }
+
+  return election;
+};
+
+export const getAllElections = async () => {
+  const { data: elections } = await get(`${baseUrl}/elections`); //IMPLEMENTAR RUTA EN API
+  return elections;
+};
+
+export const getOnePost = async (electionId) => {
+  const { data: election } = await get(`${baseUrl}/elections/${electionId}`);
+  return election;
+};
