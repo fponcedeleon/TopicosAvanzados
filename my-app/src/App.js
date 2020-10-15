ec61732fb24fb536d2c2c17aa4ea5d13786afb7e
Federico Ponce de Leon <fm.poncedeleon@gmail.com>
2020-10-15 00:09:14 -0300

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,8 @@ import Navbar from './components/Navbar';
 import Election from './pages/Election';
 import Voting from './pages/Voting';
 import Home from './pages/Home'; 
-import { BrowserRouter as Route, BrowserRouter } from 'react-router-dom'; 
+// eslint-disable-next-line
+import { BrowserRouter as Router, Switch, Route, BrowserRouter } from 'react-router-dom'; 
 import './App.css';
 import VotingDetails from './pages/VotingDetails';
 
