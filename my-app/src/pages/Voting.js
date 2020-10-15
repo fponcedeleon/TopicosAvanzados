d280f8e978c38f616748cd87069325fddb102a9c
Federico Ponce de Leon <fm.poncedeleon@gmail.com>
2020-10-14 23:23:31 -0300

    unused vars

--- my-app/src/pages/Voting.js
+++ my-app/src/pages/Voting.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom';
 import '../App.css';
 // import data from '../data.js';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 import { getAllElections } from "../scripts/services/election";
 
 export default function VotingPage() {
