cfe9618c7b60945e9b40264fa5e67c0e67ac5517
EnzoCozza <cozzaenzo@gmail.com>
2020-10-19 16:44:09 -0300

    Backend for users

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,10 +2,11 @@ const test = require("./test");
 const election = require("./election");
 const option = require("./option");
 const proposal = require("./proposal");
+const user = require("./user");
 
 module.exports = {
   name: "routes",
   register: (server) => {
-    return server.route([...test, ...election, ...option, ...proposal]);
+    return server.route([...test, ...election, ...option, ...proposal, ...user]);
   },
 };
