95968905d61d2e031e9884873573cd3326443e8b
Federico Ponce de Leon <fm.poncedeleon@gmail.com>
2020-10-22 19:45:12 -0300

    link improvements

--- api/helpers/schedule/cron.js
+++ api/helpers/schedule/cron.js
@@ -6,6 +6,12 @@ const { create } = require("../../routes/sendMail/handlers");
 
 const FiveMinsExpression = '*/1 * * * *';
 
+const linkUrl = process.env.NODE_ENV === 'localhost' ? 
+                    'http://localhost:3000' : 
+                        process.env.ENVIRONMENT === 'test' ? 
+                            'https://topicos2020testing.netlify.app' :
+                            'https://topicos2020.netlify.app';
+
 const task = () => {
     cron.schedule(FiveMinsExpression, async () => {
         const inactivatedElections = await InactivateElections();
@@ -29,11 +35,12 @@ const task = () => {
             
             const cleanVotants = [...new Set(allVotants)];
             cleanVotants.forEach(async votant => {
+                const link = `${linkUrl}/votingResult/${el._id}`;
                 const params = {
                     userName: votant.firstName,
                     userEmail: votant.email,
                     electionName: el.name,
-                    electionLink: 'link',
+                    electionLink: link,
                     subject: 'Resultado de eleccion',
                     isNewElection: false,
                     endDate: null,
