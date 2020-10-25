ca50719d452f1855624de352ea6b09c8873d2287
Federico Ponce de Leon <fm.poncedeleon@gmail.com>
2020-10-25 15:53:27 -0300

    Fixed api key location

--- api/helpers/verifier/verifier.js
+++ api/helpers/verifier/verifier.js
@@ -2,7 +2,7 @@ const Verifier = require("email-verifier");
 
 const verify = (email) => {
     console.log(email);
-    let verifier = new Verifier("at_06fHyyaNQRUDMxPNQz8UcmtRnfv6s");
+    let verifier = new Verifier(process.env.EMAIL_VERIFIER_API_KEY);
     verifier.verify(email, (err, data) => {
         if (err) throw err;
         console.log(data);
