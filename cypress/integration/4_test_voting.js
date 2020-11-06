da4cb34e1df3b0bc5be81ed591f258d46766f097
Alvaro Pardo <alvarojuegos@gmail.com>
2020-11-06 00:00:14 -0300

    Fixes
    Fixed user and password used and some tests

--- cypress/integration/4_test_voting.js
+++ cypress/integration/4_test_voting.js
@@ -2,16 +2,16 @@ describe("Form test", () => {
     it("Login form", () => {
         cy.visit('https://topicos2020.netlify.app')
         cy.get('input[id=userText]')
-        .type("alvaropardo@live.com")
+        .type("topicossw2020@gmail.com")
         cy.get('input[id=passwordText]')
-        .type("45986382")
+        .type("topicos_2020")
         cy.get('Button[type=Submit').click()
         cy.wait(3000)
         cy.get('a[href*="voting"]').click()
-        cy.get('div[class="grid-votaciones"] a').first().click()
+        cy.get('thead[class="thead-dark"]').first().get('a').eq(5).click()
         cy.get('button').click()
         cy.on('window:alert',(txt)=>{
-            should('Has votado correctamente.')
+           should('Has votado correctamente.')
         })
     });
 });
